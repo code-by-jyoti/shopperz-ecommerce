c12f6989c0d6b2fc1de104e51949ccda8d2f4012
code-by-jyoti <jyotiswami225@gmail.com>
2026-05-06 18:42:03 +0530

    feat: implement live product search filtering

--- script.js
+++ script.js
@@ -22,3 +22,40 @@ document.querySelectorAll(".menu a").forEach(link => {
         menu.classList.remove("show");
     });
 });
+
+// SEARCH FUNCTIONALITY
+const searchInput = document.getElementById("input");
+
+const items = document.querySelectorAll(".items");
+
+const noProduct = document.getElementById("noProduct");
+
+searchInput.addEventListener("keyup", () => {
+    const value = searchInput.value.toLowerCase();
+    let found = false;
+
+    // AUTO scroll to products
+    document.getElementById("products").scrollIntoView({
+        behavior: "smooth"
+    });
+
+    items.forEach(item => {
+        const name = item.querySelector(".name").textContent.toLowerCase();
+
+        if (name.includes(value)) {
+            item.style.display = "block";
+            found = true;
+        }
+        else {
+            item.style.display = "none";
+        }
+    });
+
+    // show / hide "no product found"
+    if (!found && value !== "") {
+        noProduct.style.display = "block";
+    }
+    else {
+        noProduct.style.display = "none";
+    }
+});
